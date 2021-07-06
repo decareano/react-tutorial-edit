6e46f180aaa7e45722b1a4f6c61f39b5afffd17b
Marcelo Gobelli <marcelo@marcelos-mbp.gateway.sonic.net>
2021-07-05 20:09:34 -0700

    add Table and simple components in it

--- src/Table.js
+++ src/Table.js
@@ -1,15 +1,20 @@
 import React, {Component} from 'react'
 
-class Table extends Component {
-  render() {
+const TableHeader = () => {
+    return (
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Job</th>
+        </tr>
+      </thead>
+    )
+  }
+
+const TableBody = () => {
+  
     return (
       <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Job</th>
-          </tr>
-        </thead>
         <tbody>
           <tr>
             <td>Charlie</td>
@@ -30,7 +35,18 @@ class Table extends Component {
         </tbody>
       </table>
     )
-  }
+  
 }
+
+class Table extends Component {
+    render() {
+      return (
+        <table>
+          <TableHeader />
+          <TableBody />
+        </table>
+      )
+    }
+  }
 
 export default Table
