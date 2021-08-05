4d20107d7510a73bf7b94b83f749fbb4c12d5a2a
Marcelo Gobelli <marcelo@marcelos-mbp.gateway.sonic.net>
2021-08-05 12:28:23 -0700

    messed up with a broken install

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Auth0Provider } from '@auth0/auth0-react';
-
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <Auth0Provider
