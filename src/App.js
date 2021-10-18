c93d78092e547b9fb24c93826a5a8fc96dd24473
Marcelo Gobelli <mgobelli@ultragenyx.com>
2021-10-17 17:14:15 -0700

    function with props calls

--- src/App.js
+++ src/App.js
@@ -1,45 +1,33 @@
-import React, { Component, useState } from 'react';
+import React, { useState } from 'react';
 
- 
+import ChoreChart from './ChoreChart';
+import ChoreForm from './ChoreForm'
+
 const App = () => {
   
-    
+    const [choreLogs, setChoreLogs] = useState([]);
+      // addChoreLog is a local variable that has the values of all the logs...you can call it pinga but if so, it needs to change line 19
+      const addChoreLog = (log) => {
+        //spread operator keeps tab of all entries..even though they dissapear after refreshing cuz there is no db
+        let logs = [...choreLogs, log];
+        console.log(logs)
+        setChoreLogs(logs);
+    }
+
+ 
     return (
-      <div>
-        {/* //passing an object...instead of a string */}
-        <Greeting greeting={{ text: "Welcome to Reactingo" }}/>
-      </div>
+      
+      <section>
+      {/* // passing ChoreForm below which was imported at the top of the file. 
+      // addChoreLog is a variable in ChoreForm.js. it's inside a function called handleSubmit. 
+      // if you want to change it you have to make the change on ChoreForm file as well
+      */}
+      <ChoreForm addChoreLog={addChoreLog}/>
+      <ChoreChart chores={choreLogs}/>
+    </section>
+     
     );
   
 }
- 
-const Greeting = ({greeting}) => {
- 
-    //below is static data; we want to pass dynamic data instead. how?
-    
- 
-    return <h1>{greeting.text}</h1>;
-  
-}
- 
-export default App;
 
-// we started with below code and end up with above code:
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <Greeting />
-//       </div>
-//     );
-//   }
-// }
- 
-// class Greeting extends Component {
-//   render() {
-//     const greeting = 'Welcome to React';
- 
-//     return <h1>{greeting}</h1>;
-//   }
-// }
+export default App
